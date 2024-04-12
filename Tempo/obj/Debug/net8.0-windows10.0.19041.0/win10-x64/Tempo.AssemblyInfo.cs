8d57385e1f668784e9691a79422476e0559af92b
rayssa <rayssa.cardoso.gonc@gmail.com>
2024-04-12 09:10:59 -0300

    com git

--- Tempo/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Tempo.AssemblyInfo.cs
+++ Tempo/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Tempo.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tempo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+396605e635ec54d0a6d75aa6d0a78e597a16a362")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tempo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tempo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
