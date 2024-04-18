14e005108080098d0e9c840cec4a98b5adc7fc5b
rayssa <rayssa.cardoso.gonc@gmail.com>
2024-04-18 11:46:11 -0300

    continuou

--- Tempo/Results.cs
+++ Tempo/Results.cs
@@ -1,6 +1,21 @@
 namespace Tempo;
 
 public class Results
-{
-    
+{  
+ public int Temp {get; set;} 
+
+ public string Description {get;set;}
+
+ public string Currently {get;set;}
+
+ public string City  {get;set;}
+
+ public int Humidity {get;set;}
+
+ public double Rain {get;set;}
+
+ public string Sunrise {get;set;}
+
+ public string Wind Speedy {get;set;}
+
 }
